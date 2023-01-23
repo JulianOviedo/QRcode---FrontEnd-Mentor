d576570d87e2c68cdcf56ed878837684afa4571c
JulianOviedo <julianovie234@gmail.com>
2023-01-23 19:08:44 -0300

    squeleton

--- qrcode/src/pages/index.js
+++ qrcode/src/pages/index.js
@@ -1,6 +1,13 @@
 import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
 
 
 export default function Home() {
- 
+  return(
+    <div className={styles.card}>
+      <Image className={styles['card-qr']}></Image>
+      <h1 className={styles['card-title']}></h1>
+      <p className={styles['card-description']}></p>
+    </div>
+  )
 }
