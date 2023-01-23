623fb3162429f8daf1d9766c3d2d16f7fa698d85
JulianOviedo <julianovie234@gmail.com>
2023-01-23 19:11:16 -0300

    src img

--- qrcode/src/pages/index.js
+++ qrcode/src/pages/index.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Home() {
   return(
     <div className={styles.card}>
-      <Image className={styles['card-qr']}></Image>
+      <Image src='/image-qr-code.png' width={50} height={50} className={styles['card-qr']}></Image>
       <h1 className={styles['card-title']}></h1>
       <p className={styles['card-description']}></p>
     </div>
