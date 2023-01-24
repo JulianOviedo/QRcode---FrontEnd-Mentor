98b82cb98cce6e2a3759fc11a96aa6c93d0e08da
JulianOviedo <julianovie234@gmail.com>
2023-01-24 10:59:56 -0300

    alt img prop

--- qrcode/src/pages/index.js
+++ qrcode/src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.card}>
         <div className={styles['card-qr__container']}>
-          <Image src='/image-qr-code.png' fill className={styles['card-qr']}></Image>
+          <Image alt='QRimg' src='/image-qr-code.png' fill className={styles['card-qr']}></Image>
         </div>
         <div className={styles['card-info--container']}>
         <h1 className={styles['card-info--title']}>Improve your front-end skills by building projects</h1>
