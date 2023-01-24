f13683085c0b6962c86c705141c01637c9f775c4
JulianOviedo <julianovie234@gmail.com>
2023-01-24 10:50:53 -0300

    fully design completed

--- qrcode/src/pages/index.js
+++ qrcode/src/pages/index.js
@@ -3,11 +3,17 @@ import Image from 'next/image'
 
 
 export default function Home() {
-  return(
-    <div className={styles.card}>
-      <Image src='/image-qr-code.png' width={50} height={50} className={styles['card-qr']}></Image>
-      <h1 className={styles['card-title']}></h1>
-      <p className={styles['card-description']}></p>
-    </div>
+  return (
+    <main className={styles.main}>
+      <div className={styles.card}>
+        <div className={styles['card-qr__container']}>
+          <Image src='/image-qr-code.png' fill className={styles['card-qr']}></Image>
+        </div>
+        <div className={styles['card-info--container']}>
+        <h1 className={styles['card-info--title']}>Improve your front-end skills by building projects</h1>
+        <p className={styles['card-info--description']}>Scan the QR code to visit FrontEnd Mentor and take your coding skills to the next level.</p>
+        </div>
+      </div>
+    </main>
   )
 }
