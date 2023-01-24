d40e58ed374dcb61f4a6a645f1fe0e46eb15c14d
JulianOviedo <julianovie234@gmail.com>
2023-01-24 10:48:53 -0300

    full design complete

--- qrcode/src/pages/_app.js
+++ qrcode/src/pages/_app.js
@@ -1,5 +1,16 @@
+import { Outfit } from '@next/font/google'
 import '@/styles/globals.css'
 
+const outfit = Outfit({
+  subsets: ['latin'],
+  weight: '400'
+})
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={outfit.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+
 }
